a0503252917c2cf8a21d212a8facf5edc3176a97
Surya <suryasekhardaripa60461@gmail.com>
2023-02-07 12:34:26 +0530

    done

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,38 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] matrix=new int[rows][columns];
+        int count=1;
+        int startrow=0,startcol=0;
+        int endrow=rows-1;
+        int endcol=columns-1;
+        while(startrow<=endrow && startcol<=endcol){
+            for(int i=startcol;i<=endcol;i++){
+                matrix[startrow][i]=count;
+                count++;
+            }
+            if(count>rows*columns){
+                break;
+            }
+            else{
+            startrow++;
+            for(int j=startrow;j<=endrow;j++){
+                matrix[j][endcol]=count;
+                count++;
+            }
+            endcol--;
+            for(int k=endcol;k>=startcol;k--){
+                matrix[endrow][k]=count;
+                count++;
+            }
+            endrow--;
+            for(int l=endrow;l>=startrow;l--){
+                matrix[l][startcol]=count;
+                count++;
+            }
+            startcol++;
+        }
+    }
+        return matrix;
     }
 }
